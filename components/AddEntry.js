7bae811893c63a8d34c08901320f00eb64a75d1c
Renington Neri <renington.neri@gmail.com>
2017-12-12 08:54:13 -0200

    Changes for AddEntry-methods

---
+++ components/AddEntry.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  }
+  
+  render() {
+    return (
+      <View>
+        {getMetricMetaInfo('bike').getIcon()}
+      </View>
+    )
+  }
+}
