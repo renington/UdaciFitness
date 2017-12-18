375383c59ec80f33e452bb005e6c7af8fe5c8139
Renington Neri <renington.neri@gmail.com>
2017-12-18 08:07:14 -0200

    feat: adding actions methods in AddEntry Component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -10,6 +10,36 @@ export default class AddEntry extends Component {
     sleep: 0,
     eat: 0,
   }
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric)
+
+    this.setState((state) => {
+      const count  = state[metric]
+
+      return {
+        ...state,
+        [metric]: count > max ? max : count
+      }
+    })
+  }
+
+  decrement = (metric) => {
+    this.setState((state) => {
+      const count  = state[metric] - getMetricMetaInfo(metric).step
+
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count
+      }
+    })
+  }
+
+  slide = (metric, value) => {
+    this.setState(() => ({
+      [metric]: value
+    }))
+  }
   
   render() {
     return (
